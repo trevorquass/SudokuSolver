f2f688a54cd728a756768517bfd66a4688e2e8db
unknown <TQuass_be@dev1.devcodecamp.inc>
2015-12-17 12:14:20 -0600

    added solution to repo

---
+++ SudokuSolverProject/SudokuSolverProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolverProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //new SudokuSolver("850002400" + "720000009" + "004000000" + "000107002" + "305000900" + "040000000" + "000080070" + "017000000" + "000036040").Solve();
+            //new SudokuSolver("200080300" + "060070084" + "030500209" + "000105408" + "000000000" + "402706000" + "301007040" + "720040060" + "004010003").Solve();
+            new SudokuSolver("200080300060070084030500209000105408000000000402706000301007040720040060004010003").Solve();
+            Console.Read();
+        }
+    }
+}
---
+++ SudokuSolverProject/SudokuSolverProject/SudokuSolver.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolverProject
+{
+    class SudokuSolver
+    {
+        private int[] grid;
+
+        public SudokuSolver(String s)
+        {
+            grid = new int[81];
+            for (int i = 0; i < s.Length; i++)
+            {
+                grid[i] = int.Parse(s[i].ToString());
+            }
+        }
+
+        public void Solve()
+        {
+            try
+            {
+                PlaceNumber(0);
+                Console.WriteLine("Cannot Solve");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(this);
+            }
+            //PlaceNumber(0);
+        }
+
+        public void PlaceNumber(int position)
+        {
+            if (position == 81)
+            {
+                throw new Exception("Solved");
+                //Console.WriteLine("Solved");
+            }
+            if (grid[position] > 0)
+            {
+                PlaceNumber(position + 1);
+                return;
+            }
+            for (int n = 1; n <= 9; n++)
+            {
+                if (CheckIfValid(n, position % 9, position / 9))
+                {
+                    grid[position] = n;
+                    PlaceNumber(position + 1);
+                    grid[position] = 0;
+                }
+            }
+        }
+
+        public bool CheckIfValid(int val, int x, int y)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                if (grid[y * 9 + i] == val || grid[i * 9 + x] == val)
+                    return false;
+            }
+            int startX = (x / 3) * 3;
+            int startY = (y / 3) * 3;
+            for (int i = startY; i < startY + 3; i++)
+            {
+                for (int j = startX; j < startX + 3; j++)
+                {
+                    if (grid[i * 9 + j] == val)
+                        return false;
+                }
+            }
+            return true;
+        }
+
+        public override string ToString()
+        {
+            string sb = "";
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    sb += (grid[i * 9 + j] + " ");
+                    //if (j == 2 || j == 5) ;
+                        //sb += ("| ");
+                        //sb += ("");
+                }
+                sb += ('\n');
+                //if (i == 2 || i == 5)
+                //    sb += ("------+-------+------\n");
+            }
+            return sb;
+        }
+
+    }
+}
